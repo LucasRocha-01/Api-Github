07e1bdc1096c8be53c340762de8286baf8e77774
Lucas Rocha <rocha.lucas28@gmail.com>
2020-08-29 17:57:04 -0300

    mostrar informaçoes dos repositorios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
-
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories, Error } from './styles';
@@ -77,11 +77,9 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a
+          <Link
             key={repository.full_name}
-            href="teste"
-            // eslint-disable-next-line react/jsx-no-target-blank
-            target="_blank"
+            to={`/repository/${repository.full_name}`}
           >
             <img
               src={repository.owner.avatar_url}
@@ -93,7 +91,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
